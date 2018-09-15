a1a303dd352e4f656677b516c99a1d4352d6e331
Andiy Galas <halasandreyko@gmail.com>
2018-09-15 23:01:22 +0300

    Some chages

--- Team-AAA.Task1/Team-AAA.Task1/Readable.cs
+++ Team-AAA.Task1/Team-AAA.Task1/Readable.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Team_AAA.Task1
 {
     class Readable
     {
-
+        int f;
+        public Readable() { f = 0; }
     }
 }
