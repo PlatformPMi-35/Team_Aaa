a0cc34fa905466eda4ae7b90d456160f7021bb6f
Andriy Iatskiv <andriy.iatskiv@gmail.com>
2018-09-18 20:42:21 +0200

    Write first testes

--- Team-AAA.Task1/GitTest/UnitTest1.cs
+++ Team-AAA.Task1/GitTest/UnitTest1.cs
@@ -10,10 +10,30 @@ namespace GitTest
         public class UnitTest1
         {
             [TestMethod]
-            public void TestMethod1()
+            public void HryvnaTest()
             {
-               
+            CurrencyName a;
+            a = ConverterToCurrencyName.Convert("грн");
+            Assert.AreEqual(a, CurrencyName.UAH);
             }
+        public void DollarTest()
+        {
+            CurrencyName a;
+            a = ConverterToCurrencyName.Convert("$");
+            Assert.AreEqual(a, CurrencyName.Dollar);
         }
+        public void EuroTest()
+        {
+            CurrencyName a;
+            a = ConverterToCurrencyName.Convert("Є");
+            Assert.AreEqual(a, CurrencyName.Euro);
+        }
+        public void NoneTest()
+        {
+            CurrencyName a;
+            a = ConverterToCurrencyName.Convert("юань");
+            Assert.AreEqual(a, CurrencyName.None);
+        }
+    }
     
 }
