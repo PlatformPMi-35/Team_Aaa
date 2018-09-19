b140f5bc67a6b960878a777847cef8e5087d5f0f
Юра Хільченко <klop414@gmail.com>
2018-09-19 23:57:44 +0300

    Finished exeption

--- Team-AAA.Task1/Team-AAA.Task1/Program.cs
+++ Team-AAA.Task1/Team-AAA.Task1/Program.cs
@@ -31,7 +31,9 @@ namespace Team_AAA.Task1
             List<Currency> lcur=new List<Currency>();
             Console.WriteLine("-==All list==-");
             //
-            cur.Read(lcur);
+            try
+            {
+                cur.Read(lcur);
            
             cur.ShowALL(lcur);
             //
@@ -43,26 +45,38 @@ namespace Team_AAA.Task1
             currencies = PairOfCurrency.DoPair(lcur);
             FileManager.Save("../../Result.txt", currencies);
             Console.WriteLine("-==Succesfully saved to file==-");
-            Console.WriteLine("Choose to what currency convert all money \n1 for Dollar, 2 - Euro and 3 for UAH:");
-            choice = Convert.ToInt32(Console.ReadLine());
-            if (choice == 1)
-            {
-                resultOfConversion = Conversion.To(currencies, CurrencyName.Dollar) ;
-                FileManager.Save("../../ResultOfConversion.txt", resultOfConversion);
-            }
-            else if (choice == 2)
-            {
-                resultOfConversion = Conversion.To(currencies, CurrencyName.Euro) ;
-                FileManager.Save("../../ResultOfConversion.txt", resultOfConversion);
-            }
-            else if (choice == 3)
-            {
-                resultOfConversion = Conversion.To(currencies, CurrencyName.UAH) ;
-                FileManager.Save("../../ResultOfConversion.txt", resultOfConversion);
-
-            }
-            else throw new ArgumentNullException("Wrong choice of conversion!");
             
+            Console.WriteLine("Choose to what currency convert all money \n1 for Dollar, 2 - Euro and 3 for UAH:");
+            
+                choice = Convert.ToInt32(Console.ReadLine());
+
+
+
+                if (choice == 1)
+                {
+                    resultOfConversion = Conversion.To(currencies, CurrencyName.Dollar);
+                    FileManager.Save("../../ResultOfConversion.txt", resultOfConversion);
+                }
+                else if (choice == 2)
+                {
+                    resultOfConversion = Conversion.To(currencies, CurrencyName.Euro);
+                    FileManager.Save("../../ResultOfConversion.txt", resultOfConversion);
+                }
+                else if (choice == 3)
+                {
+                    resultOfConversion = Conversion.To(currencies, CurrencyName.UAH);
+                    FileManager.Save("../../ResultOfConversion.txt", resultOfConversion);
+
+                }
+                else throw new ArgumentNullException("Wrong choice of conversion!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                //Console.ReadKey();
+                
+                
+            }
 
             Console.ReadKey();
             //Testing
