7a8d63c6cdc51393919ce389f7d567bee3653218
Galas Andriy <halasandreyko@gmail.com>
2018-10-30 23:42:12 +0200

    Wrote classes Sushi,CollectionOfShushi and interface IFileManager

---
+++ Task3/Task3/Classes/CollectionOfShushi.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task3.Classes
+{
+    class CollectionOfShushi:Sushi,IFileManager
+    {
+        List<Sushi> list;
+
+        public CollectionOfShushi()
+        {
+            list = new List<Sushi>();
+        }
+ 
+        public void Read(string filePath)
+        {
+            StreamReader Reader = new StreamReader(filePath, System.Text.Encoding.Default);
+            string sLine = Reader.ReadLine();
+            while (sLine != null)
+            {
+                string[] fields = sLine.Split();
+                list.Add(new Sushi(fields[0], Convert.ToDouble(fields[1]), Convert.ToDouble(fields[2]), fields[3]));
+                sLine = Reader.ReadLine();
+            }
+            Reader.Close();
+        }
+
+        public void Write(string filePath)
+        {
+            if (filePath == null)
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            using (var writer = new StreamWriter(filePath))
+            {
+                foreach (var i in list)
+                {
+                    string h = i.ToWrite();
+                    writer.WriteLine(h);
+                }
+            }
+        }
+
+        public void Add(Sushi toAdd)
+        {
+            if(list.Find(x=>x.name==toAdd.name)!=null||!toAdd.isGood())
+            {
+                throw new ArgumentNullException("This item is already in list or it is empty!");
+            }
+            else
+            {
+                list.Add(toAdd);
+            }
+        }
+
+        public void DeleteByName(string nameToDelete)
+        {
+            foreach(var i in list)
+            {
+                if(i.name==nameToDelete)
+                {
+                    list.Remove(i);
+                }
+            }
+        }
+
+        public List<Sushi> getList()
+        {
+            return list;
+        }
+    }
+}
---
+++ Task3/Task3/Classes/Sushi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Classes
+{
+    class Sushi
+    {
+
+        internal string name { get; set; }
+
+        double price { get; set; }
+
+        double weight { get; set; }
+
+        string ingredients { get; set; }
+
+        public Sushi()
+        {
+            ingredients =string.Empty;
+            price =double.NaN;
+            name = string.Empty;
+            weight = double.NaN;
+        }
+
+        public Sushi(string nm, double pr,double wght, string ing)
+        {
+            ingredients = ing;
+            price = pr;
+            name = nm;
+            weight = wght;
+
+        }
+
+        protected internal bool isGood()
+        {
+            if(this.name!=string.Empty&&this.price!=double.NaN&&this.weight!=double.NaN&&this.ingredients!=string.Empty)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return name + " " + price + "$ " + weight + "g " + ingredients;
+        }
+
+        public string ToWrite()
+        {
+            return name + " " + price + " " + weight + " " + ingredients;
+        }
+
+    }
+}
