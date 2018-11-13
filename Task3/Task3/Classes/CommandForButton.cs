b786509330f40c7913747d99ab4df69375f0e4f0
Yaroslav <drtonygames@gmail.com>
2018-11-13 05:41:03 +0200

    some xml documantation

--- Task3/Task3/Classes/CommandForButton.cs
+++ Task3/Task3/Classes/CommandForButton.cs
@@ -9,9 +9,17 @@ namespace Task3.Classes
 {
     class CommandForButton
     {
+        /// <summary>
+        /// Button delete for get value
+        /// </summary>
         public static RoutedCommand Delete { get; set; }
+        /// <summary>
+        /// Button tolist for get list
+        /// </summary>
         public static RoutedCommand ToList { get; set; }
-
+        /// <summary>
+        /// Some method what can with commands
+        /// </summary>
         static CommandForButton()
         {
             Delete = new RoutedCommand("Delete", typeof(MainWindow));
--- Task3/Task3/Classes/FileManager.cs
+++ Task3/Task3/Classes/FileManager.cs
@@ -7,11 +7,20 @@ using System.IO;
 
 namespace Task3.Classes
 {
-
+    /// <summary>
+    /// Interface FileManager for control some file elems and had some value
+    /// </summary>
     interface IFileManager
     {
+        /// <summary>
+        /// Method witch read file
+        /// </summary>
+        /// <param name="filePath"></param>
         void Read(string filePath);
-
+        /// <summary>
+        /// Method witch write to file
+        /// </summary>
+        /// <param name="filePath"></param>
         void Write(string filePath);
     }
 
